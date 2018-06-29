cce93e2d1cc98981f64bdb39dde4176c71f4e14c
Guilherme Duarte <40505834+guilzard@users.noreply.github.com>
2018-06-29 20:18:49 -0300

    Update AulaGit.java

--- src/aulagit/AulaGit.java
+++ src/aulagit/AulaGit.java
@@ -10,9 +10,9 @@ package aulagit;
  * @author Alunos
  */
 public class AulaGit {
-
+    private int  a;
     /**
-     * @param args the command line arguments
+     * @param args the command line argumentsteste
      */
     public static void main(String[] args) {
         // TODO code application logic here
